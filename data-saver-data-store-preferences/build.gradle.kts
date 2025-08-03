cc6b696639982267cae29b3f4c0877ccb3e2221f
FunnySaltyFish <3036548996@qq.com>
2025-08-03 21:39:57 +0800

    remove redundant `publishing` settings in two Android modules

--- data-saver-data-store-preferences/build.gradle.kts
+++ data-saver-data-store-preferences/build.gradle.kts
@@ -32,10 +32,6 @@ android {
     namespace = "com.funny.data_saver_data_store"
 }
 
-tasks.register<Jar>("generateSourcesJar") {
-    from(android.sourceSets["main"].java.srcDirs)
-}
-
 dependencies {
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.runtime)
@@ -47,38 +43,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                // Applies the component for the release build variant.
-                from(components["release"])
-
-                // You can then customize attributes of the publication as shown below.
-                groupId = libs.versions.group.get()
-                artifactId = "data-saver-data-store-preferences"
-                version = libs.versions.project.get()
-                artifact(tasks["generateSourcesJar"])
-            }
-        }
-    }
-
-    // 设置所有的 publish 任务 需要在 sign 之后
-    // 我也不知道为什么需要手动这么写，但是 Gradle 一直报错，只好按着报错一点点尝试
-    // 最后写出了这一堆。。。
-    val signTasks = tasks.filter { it.name.startsWith("sign") && it.name != "sign"}
-
-    // project.logger.warn(signTasks.joinToString { it.name + ", " })
-    tasks.configureEach {
-        // project.logger.warn("task name: $name")
-        if (!name.startsWith("publish")) return@configureEach
-        if (name == "publish") return@configureEach
-
-        signTasks.forEach { signTask ->
-            this.dependsOn(signTask)
-        }
-    }
 }
